fecc814b8f93b380d654c58e8a010e2803deacd9
marceloteclas <mdj3114@gmail.com>
2025-04-07 09:51:56 -0300

    Organização/Front e controlers feitos.

---
+++ src/main/java/com/System/clinic/Dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.System.clinic.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String field;
+    private String message;
+
+}
---
+++ src/main/java/com/System/clinic/Dto/Page.java
@@ -0,0 +1,5 @@
+package com.System.clinic.Dto;
+
+public record Page(String title) {
+
+}
